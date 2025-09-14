9e1e5a37f9fca6afa1335341fe44ca01927e058e
Carlos6965 <candidocarlos149@gmail.com>
2025-09-14 00:00:44 -0300

    Adição dos controllers do cliente, endereço e telefone

---
+++ SIGO-BackEnd/SIGO/Controllers/ClienteController.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SIGO.Objects.Contracts;
+using SIGO.Services.Interfaces;
+
+namespace SIGO.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        private readonly IClienteService _clienteService;
+        private readonly Response _response;
+        private readonly IMapper _mapper;
+
+        public ClienteController(IClienteService clienteService, IMapper mapper)
+        {
+            _clienteService = clienteService;
+            _mapper = mapper;
+            _response = new Response();
+        }
+
+    }
+}
---
+++ SIGO-BackEnd/SIGO/Controllers/TelefoneController.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SIGO.Objects.Contracts;
+using SIGO.Services.Interfaces;
+
+namespace SIGO.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TelefoneController : ControllerBase
+    {
+        private readonly ITelefoneService _telefoneService;
+        private readonly Response _response;
+        private readonly IMapper _mapper;
+
+        public TelefoneController(ITelefoneService telefoneService, IMapper mapper)
+        {
+            _telefoneService = telefoneService;
+            _mapper = mapper;
+            _response = new Response();
+        }
+
+    }
+}
